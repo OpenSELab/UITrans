7390bd6e6d2a75533e4714dd921c0892cd128e06
WangChen <dawnwccc712@gmail.com>
2024-10-14 02:45:01 +0800

    整合组件和页面转译

--- ui.py
+++ ui.py
@@ -1,24 +1,91 @@
 import os
 import time
 import zipfile
+import re
 
+import gradio
 import shortuuid
 from tqdm import tqdm
 import gradio as gr
 from android.base import android_config
 from android.main import analyse_page_dict
+from core.config.config_loader import ConfigLoader
+from core.llms import LLMFactory
+from core.pilot.code_monkey import CodeMonkeyAgent
+from core.pilot.developer import DeveloperAgent
+from core.pilot.schema import BreakdownAndroidLayout, BreakdownLayoutTranslation
+
+config = ConfigLoader.from_file("config.yaml")
+component_name_pattern = re.compile(r"<\s*([\w\.]+)")
+
+
+def get_llm_client():
+    llm_client = LLMFactory.create_llm_from_config(
+        llm_client_type="openai",
+        llm_config=config.llm_config["deepseek"].model_dump(),
+        generate_config={
+            "temperature": 0.01
+        }
+    )
+    return llm_client
 
 
 # Define a function to convert Android XML components to HarmonyOS code.
 def convert_android_component_to_harmony(android_xml):
-    # Simple conversion logic (this is a placeholder, actual conversion logic should be implemented)
-    harmonyos_code = android_xml.replace("android:", "harmony:")
+    # 初始化llm
+    llm_client = get_llm_client()
+    code_monkey_agent = CodeMonkeyAgent(llm_client=llm_client)
+    breakdown_android_layout = BreakdownAndroidLayout(
+        tasks=[{
+            "done": False,
+            "description": "将安卓组件代码转译为鸿蒙ArkUI组件代码，以便在鸿蒙系统中使用。",
+            "component": {
+                "name": component_name_pattern.findall(android_xml),
+                "content": android_xml
+            }
+        }]
+    )
+    translations, agent_state = code_monkey_agent.translate_component_v1(breakdown_android_layout)
+    harmonyos_code = translations[0].target_component_code
     return harmonyos_code
 
 
 def convert_android_page_to_harmony(android_xml):
-    # Simple conversion logic (this is a placeholder, actual conversion logic should be implemented)
-    harmonyos_code = android_xml.replace("android:", "harmony:")
+    # 初始化llm
+    tqdm_progress = tqdm(total=100, desc="转译进度")
+    llm_client = get_llm_client()
+    code_monkey_agent = CodeMonkeyAgent(llm_client=llm_client)
+    developer_agent = DeveloperAgent(llm_client=llm_client)
+    android_layout_xml_name = "example.xml"
+    android_layouts = {
+        android_layout_xml_name: {
+            "name": android_layout_xml_name,
+            "content": android_xml
+        }
+    }
+    layout_translation = BreakdownLayoutTranslation(
+        tasks=[{
+            "description": f"将{android_layout_xml_name}转译为对应的Harmony页面",
+            "done": False,
+            "android": android_layout_xml_name,
+            "harmony": "ets/pages/Index.ets"
+        }]
+    )
+    tqdm_progress.update(10)
+    breakdown_android_layout = developer_agent.breakdown_android_layout(layout_translation, android_layouts)
+    tqdm_progress.update(30)
+    translations, agent_state = code_monkey_agent.translate_component_v1(breakdown_android_layout)
+    tqdm_progress.update(80)
+    code_block_pattern = r"```(?:\w+)?\n([\s\S]*?)```"
+    assemble_result = developer_agent.assemble_harmony_layout(breakdown_android_layout,
+                                                              android_layouts[android_layout_xml_name], translations)
+    tqdm_progress.update(100)
+    code_block_match = re.search(code_block_pattern, assemble_result)
+    if code_block_match:
+        harmonyos_code = code_block_match.group(1)
+    else:
+        harmonyos_code = assemble_result
+    tqdm_progress.close()
     return harmonyos_code
 
 
@@ -82,18 +149,35 @@ with gr.Blocks() as app:
         android_xml_input = gr.TextArea(label="输入 Android XML 组件", lines=20, max_lines=100)
         convert_button = gr.Button("确定")
         harmonyos_output = gr.TextArea(label="输出 HarmonyOS 代码", lines=10, max_lines=100)
-
+        component_progress = gr.Progress(track_tqdm=True)
         # Define the event listener for the button click.
         convert_button.click(convert_android_component_to_harmony, inputs=android_xml_input, outputs=harmonyos_output)
+
+        component_examples = gr.Examples(
+            examples=[
+                "<CheckBox\n        android:id=\"@+id/chbOne\"\n        android:layout_width=\"wrap_content\"\n        android:layout_height=\"wrap_content\"\n        android:checked=\"true\"\n        android:text=\"吃饭\" />",
+                "<RatingBar\n        android:id=\"@+id/rating1\"\n        android:layout_width=\"wrap_content\"\n        android:layout_height=\"wrap_content\"\n        android:numStars=\"5\"\n        android:stepSize=\"0.5\"\n        android:rating=\"3\"\n        android:isIndicator=\"false\" />",
+                "<EditText\nandroid:id=ght=\"50dp\"\nandroid:layout_marginBottom=\"15dp\"\nandroid:autofillHints=\"\"\nandroid:hint=\"请输入文字\"\nandroid:inputType=\"text\"\nandroid:paddingLeft=\"15dp\"\nandroid:paddingRight=\"15dp\"\nandroid:textColor=\"@color/black\"\nandroid:textSize=\"16sp\" />"
+            ],
+            inputs=[android_xml_input],
+        )
     with gr.Tab("页面转译"):
         gr.Markdown("## 页面转译")
         gr.Markdown("请在下列输入框中粘贴 Android XML 页面代码，然后点击“确定”按钮以将其转译为 HarmonyOS 代码。")
         android_xml_input = gr.TextArea(label="输入 Android XML 页面", lines=20, max_lines=100)
         convert_button = gr.Button("确定")
         harmonyos_output = gr.TextArea(label="输出 HarmonyOS 代码", lines=10, max_lines=100)
-
+        page_progress = gr.Progress(track_tqdm=True)
         # Define the event listener for the button click.
         convert_button.click(convert_android_page_to_harmony, inputs=android_xml_input, outputs=harmonyos_output)
+
+        page_examples = gr.Examples(
+            examples=[
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<androidx.core.widget.NestedScrollView xmlns:android=\"http://schemas.android.com/apk/res/android\"\n    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n    xmlns:tools=\"http://schemas.android.com/tools\"\n    android:layout_width=\"match_parent\"\n    android:layout_height=\"match_parent\"\n    tools:context=\".FirstFragment\">\n\n    <androidx.constraintlayout.widget.ConstraintLayout\n        android:layout_width=\"match_parent\"\n        android:layout_height=\"match_parent\"\n        android:padding=\"16dp\">\n\n        <Button\n            android:id=\"@+id/button_first\"\n            android:layout_width=\"wrap_content\"\n            android:layout_height=\"wrap_content\"\n            android:text=\"@string/next\"\n            app:layout_constraintBottom_toTopOf=\"@id/textview_first\"\n            app:layout_constraintEnd_toEndOf=\"parent\"\n            app:layout_constraintStart_toStartOf=\"parent\"\n            app:layout_constraintTop_toTopOf=\"parent\" />\n\n        <TextView\n            android:id=\"@+id/textview_first\"\n            android:layout_width=\"wrap_content\"\n            android:layout_height=\"wrap_content\"\n            android:layout_marginTop=\"16dp\"\n            android:text=\"@string/lorem_ipsum\"\n            app:layout_constraintBottom_toBottomOf=\"parent\"\n            app:layout_constraintEnd_toEndOf=\"parent\"\n            app:layout_constraintStart_toStartOf=\"parent\"\n            app:layout_constraintTop_toBottomOf=\"@id/button_first\" />\n    </androidx.constraintlayout.widget.ConstraintLayout>\n</androidx.core.widget.NestedScrollView>",
+                "<androidx.coordinatorlayout.widget.CoordinatorLayout\n    xmlns:android=\"http://schemas.android.com/apk/res/android\"\n    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n    android:layout_width=\"match_parent\"\n    android:layout_height=\"match_parent\"\n    android:fitsSystemWindows=\"true\">\n\n    <ImageView\n        android:layout_width=\"match_parent\"\n        android:layout_height=\"match_parent\"\n        android:scaleType=\"centerCrop\"\n        android:src=\"@drawable/appbackground\"/>\n\n    <com.google.android.material.appbar.AppBarLayout\n        android:layout_width=\"match_parent\"\n        android:layout_height=\"?attr/actionBarSize\"\n        android:fitsSystemWindows=\"true\">\n\n        <androidx.appcompat.widget.Toolbar\n            android:id=\"@+id/toolbar\"\n            android:layout_width=\"match_parent\"\n            android:layout_height=\"wrap_content\"\n            android:background=\"?attr/colorPrimary\"\n            android:fitsSystemWindows=\"true\"\n            android:minHeight=\"?attr/actionBarSize\"\n            android:title=\"@string/action_about\"\n            app:layout_scrollFlags=\"scroll|enterAlways\"\n            app:popupTheme=\"@style/ThemeOverlay.AppCompat.Light\"\n            app:theme=\"@style/ThemeOverlay.AppCompat.Dark.ActionBar\"\n            />\n\n\n    </com.google.android.material.appbar.AppBarLayout>\n\n    <ScrollView\n        android:id=\"@+id/nestedScrollView\"\n        android:layout_width=\"match_parent\"\n        android:layout_height=\"match_parent\"\n        android:layout_marginTop=\"?attr/actionBarSize\"\n        android:fitsSystemWindows=\"true\"\n        >\n\n        <LinearLayout\n            android:layout_width=\"match_parent\"\n            android:layout_height=\"wrap_content\"\n            android:orientation=\"vertical\"\n            android:paddingBottom=\"@dimen/activity_vertical_margin\"\n            android:paddingTop=\"@dimen/activity_vertical_margin\">\n\n            <androidx.cardview.widget.CardView\n                    android:layout_width=\"match_parent\"\n                    android:layout_height=\"wrap_content\"\n                    android:layout_marginBottom=\"4dp\"\n                    android:layout_marginLeft=\"@dimen/activity_horizontal_margin\"\n                    android:layout_marginRight=\"@dimen/activity_horizontal_margin\"\n\n                    android:layout_marginTop=\"4dp\"\n                    android:padding=\"16dp\"\n                    app:cardCornerRadius=\"8dp\">\n\n                <LinearLayout\n                        android:layout_width=\"match_parent\"\n                        android:layout_height=\"wrap_content\"\n                        android:orientation=\"vertical\">\n\n                    <TextView\n                            android:id=\"@+id/text_view_about\"\n                            android:layout_width=\"wrap_content\"\n                            android:layout_height=\"wrap_content\"\n                            android:layout_marginBottom=\"24dp\"\n                            android:layout_marginLeft=\"16dp\"\n                            android:layout_marginRight=\"16dp\"\n                            android:layout_marginTop=\"16dp\"\n                            android:breakStrategy=\"high_quality\"\n                            android:hyphenationFrequency=\"full\"\n                            android:text=\"@string/heading_about\"\n                            android:textAppearance=\"?android:attr/textAppearanceMedium\"\n                            android:textIsSelectable=\"true\"\n                            android:textSize=\"16sp\">\n\n                        <requestFocus/>\n                    </TextView>\n                </LinearLayout>\n            </androidx.cardview.widget.CardView>\n\n            <androidx.cardview.widget.CardView\n                    android:layout_width=\"match_parent\"\n                    android:layout_height=\"wrap_content\"\n                    android:layout_marginBottom=\"4dp\"\n                    android:layout_marginLeft=\"@dimen/activity_horizontal_margin\"\n                    android:layout_marginRight=\"@dimen/activity_horizontal_margin\"\n\n                    android:layout_marginTop=\"4dp\"\n                    android:padding=\"16dp\"\n                    app:cardCornerRadius=\"8dp\">\n\n                <LinearLayout\n                        android:layout_width=\"match_parent\"\n                        android:layout_height=\"wrap_content\"\n                        android:orientation=\"vertical\"\n                        android:padding=\"8dp\">\n\n                    <ImageView\n                            android:id=\"@+id/imageViewLogo\"\n                            android:layout_width=\"wrap_content\"\n                            android:layout_height=\"200dp\"\n                            android:layout_gravity=\"center\"\n                            android:layout_marginBottom=\"8dp\"\n                            android:layout_marginLeft=\"8dp\"\n                            android:layout_marginRight=\"8dp\"\n                            android:layout_marginTop=\"8dp\"\n                            app:srcCompat=\"@drawable/bookdash_logo\"/>\n\n\n                </LinearLayout>\n\n            </androidx.cardview.widget.CardView>\n\n            <androidx.cardview.widget.CardView\n                    android:layout_width=\"match_parent\"\n                    android:layout_height=\"wrap_content\"\n                    android:layout_marginBottom=\"4dp\"\n                    android:layout_marginLeft=\"@dimen/activity_horizontal_margin\"\n                    android:layout_marginRight=\"@dimen/activity_horizontal_margin\"\n\n                    android:layout_marginTop=\"4dp\"\n                    android:padding=\"16dp\"\n                    app:cardCornerRadius=\"8dp\">\n\n                <LinearLayout\n                        android:layout_width=\"match_parent\"\n                        android:layout_height=\"wrap_content\"\n                        android:orientation=\"vertical\">\n\n                    <TextView\n                            android:id=\"@+id/textViewHeading\"\n                            android:layout_width=\"wrap_content\"\n                            android:layout_height=\"wrap_content\"\n                            android:layout_marginBottom=\"8dp\"\n                            android:layout_marginLeft=\"16dp\"\n                            android:layout_marginRight=\"16dp\"\n                            android:layout_marginTop=\"16dp\"\n                            android:text=\"@string/why_bookdash_heading\"\n                            android:textAppearance=\"?android:attr/textAppearanceLarge\"\n                            android:textIsSelectable=\"true\"\n                            android:textSize=\"24sp\"/>\n\n                    <TextView\n                            android:id=\"@+id/text_why_bookdash\"\n                            style=\"@style/TextAppearance.AppCompat.Medium\"\n                            android:layout_width=\"wrap_content\"\n                            android:layout_height=\"wrap_content\"\n                            android:layout_marginBottom=\"16dp\"\n\n                            android:layout_marginLeft=\"16dp\"\n                            android:layout_marginRight=\"16dp\"\n                            android:breakStrategy=\"high_quality\"\n                            android:fontFamily=\"sans-serif\"\n                            android:hyphenationFrequency=\"normal\"\n                            android:text=\"@string/why_bookdash\"\n                            android:textIsSelectable=\"true\"\n                            android:textSize=\"16sp\"/>\n\n                    <Button\n                            android:id=\"@+id/button_learn_more\"\n                            style=\"@style/Widget.AppCompat.Button.Borderless\"\n                            android:layout_width=\"wrap_content\"\n                            android:layout_height=\"wrap_content\"\n                            android:layout_marginBottom=\"8dp\"\n                            android:layout_marginLeft=\"8dp\"\n                            android:layout_marginRight=\"8dp\"\n                            android:fontFamily=\"sans-serif\"\n                            android:text=\"@string/learn_more\"\n                            android:textColor=\"@color/colorAccent\"/>\n                </LinearLayout>\n\n            </androidx.cardview.widget.CardView>\n\n\n        </LinearLayout>\n\n\n    </ScrollView>\n\n</androidx.coordinatorlayout.widget.CoordinatorLayout>\n\n\n"
+            ],
+            inputs=[android_xml_input],
+        )
     with gr.Tab("项目转译"):
         gr.Markdown("## 项目转译")
         gr.Markdown("请在下列文件上传框中上传 Android 项目的 Zip 压缩包，然后点击“确定”按钮以将其转译为 HarmonyOS 代码。")
@@ -101,6 +185,7 @@ with gr.Blocks() as app:
         convert_button = gr.Button("确定", interactive=True)
         message_output = gr.Text(label="转译进度", interactive=False, lines=1, max_lines=1, autoscroll=True)
         download_button = gr.Button("下载", interactive=True)
+        project_progress_bar = gr.Progress(track_tqdm=True)
 
         # 存储解析后的内容
         android_xml_parsed_content = ""
@@ -133,4 +218,5 @@ with gr.Blocks() as app:
 
 # Launch the Gradio app.
 if __name__ == "__main__":
-    app.launch(show_error=True, share=True, show_api=False)
+    # 设置队列最大为20，最大并发数为3
+    app.queue(max_size=20, default_concurrency_limit=3).launch(show_error=True, share=True, show_api=False)
